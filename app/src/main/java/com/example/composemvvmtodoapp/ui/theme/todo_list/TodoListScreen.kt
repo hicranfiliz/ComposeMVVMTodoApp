f56096ef4b839c2dfc7d17b7df10ebb52d596b7d
hicranfiliz <h.filiz781@gmail.com>
2025-02-17 23:24:36 +0300

    created todo list screen composable

---
+++ app/src/main/java/com/example/composemvvmtodoapp/ui/theme/todo_list/TodoListScreen.kt
@@ -0,0 +1,36 @@
+package com.example.composemvvmtodoapp.ui.theme.todo_list
+
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.traceEventEnd
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.composemvvmtodoapp.util.UiEvent
+
+@Composable
+fun TodoListScreen(
+    onNavigate: (UiEvent.Navigate) -> Unit,
+    viewModel: TodoListViewModel = hiltViewModel()
+) {
+    val todos = viewModel.todos.collectAsState(initial = emptyList())
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    LaunchedEffect(key1 = true) {
+        viewModel.uiEvent.collect{ event ->
+            when(event){
+                is UiEvent.ShowSnackbar -> {
+                    snackbarHostState.showSnackbar(
+                        message = event.message,
+                        actionLabel = event.action
+                    )
+                }
+                is UiEvent.Navigate -> onNavigate(event)
+                else -> Unit
+            }
+
+        }
+    }
+}
